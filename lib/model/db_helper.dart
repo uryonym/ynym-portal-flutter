b19ebfb8eb60bde3312b6b5adfa39c9a8233e272
yoneyama ryo <uryonym@gmail.com>
2023-11-29 21:33:57 +0900

    タスク関連のDBヘルパーを修正

--- lib/model/db_helper.dart
+++ lib/model/db_helper.dart
@@ -2,18 +2,6 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'task.dart';
 
-const String columnId = '_id';
-const String columnTitle = 'title';
-const String columnIsComplete = 'isComplete';
-const String columnCreatedAt = 'createdAt';
-
-const List<String> columns = [
-  columnId,
-  columnTitle,
-  columnIsComplete,
-  columnCreatedAt,
-];
-
 class DbHelper {
   static final DbHelper instance = DbHelper._createInstance();
   static Database? _database;
@@ -25,7 +13,7 @@ class DbHelper {
   }
 
   Future<Database> _initDB() async {
-    String path = join(await getDatabasesPath(), 'tasks.db');
+    String path = join(await getDatabasesPath(), 'ynymportal.db');
 
     return await openDatabase(path, version: 1, onCreate: _onCreate);
   }
@@ -33,41 +21,38 @@ class DbHelper {
   Future _onCreate(Database database, int version) async {
     await database.execute('''
       CREATE TABLE tasks(
-        _id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title TEXT,
-        isComplete BOOLEAN,
-        createdAt TEXT
+        id TEXT PRIMARY KEY,
+        title TEXT
       )
     ''');
   }
 
-  Future<List<Task>> selectAllTasks() async {
+  Future<List<Task>> getTasks() async {
     final db = await instance.database;
-    final tasksData = await db.query('tasks');
+    final tasks = await db.query('tasks');
 
-    return tasksData.map((json) => Task.fromJson(json)).toList();
+    return tasks.map((json) => Task.fromMap(json)).toList();
   }
 
-  Future<Task> taskData(int id) async {
+  Future<Task> getTask(String id) async {
     final db = await instance.database;
-    var task = [];
-    task = await db
-        .query('tasks', columns: columns, where: '_id = ?', whereArgs: [id]);
+    final List<Map<String, Object?>> task = await db
+        .query('tasks', where: '_id = ?', whereArgs: [id]);
 
-    return Task.fromJson(task.first);
+    return Task.fromMap(task.first);
   }
 
   Future insert(Task task) async {
     final db = await database;
 
-    return await db.insert('tasks', task.toJson());
+    return await db.insert('tasks', task.toMap());
   }
 
   Future update(Task task) async {
     final db = await database;
 
     return await db
-        .update('tasks', task.toJson(), where: '_id = ?', whereArgs: [task.id]);
+        .update('tasks', task.toMap(), where: '_id = ?', whereArgs: [task.id]);
   }
 
   Future delete(int id) async {
