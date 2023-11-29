51a95a3097dcfa56d8e882ba99cb3c99bae52d96
yoneyama ryo <uryonym@gmail.com>
2023-11-29 21:17:24 +0900

    Taskモデルの定義を修正、パッケージを整理

--- lib/model/task.dart
+++ lib/model/task.dart
@@ -1,41 +1,16 @@
-import 'db_helper.dart';
-import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
 
 class Task {
-  int? id;
-  String title;
-  bool isComplete;
-  DateTime createdAt;
+  late String id;
+  late String title;
 
   Task({
-    this.id,
+    required this.id,
     required this.title,
-    required this.isComplete,
-    required this.createdAt,
   });
 
-  Task copy({
-    int? id,
-    String? title,
-    bool? isComplete,
-    DateTime? createdAt,
-  }) =>
-      Task(
-          id: id ?? this.id,
-          title: title ?? this.title,
-          isComplete: isComplete ?? this.isComplete,
-          createdAt: createdAt ?? this.createdAt);
-
-  static Task fromJson(Map<String, Object?> json) => Task(
-        id: json[columnId] as int,
-        title: json[columnTitle] as String,
-        isComplete: json[columnIsComplete] as bool,
-        createdAt: DateTime.parse(json[columnCreatedAt] as String),
-      );
-
-  Map<String, Object> toJson() => {
-        columnTitle: title,
-        columnIsComplete: isComplete,
-        columnCreatedAt: DateFormat('yyyy-MM-dd HH:mm:ss').format(createdAt),
-      };
+  Task.newTask() {
+    id = const Uuid().v4();
+    title = "";
+  }
 }
